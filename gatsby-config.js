a948c41d3d58144f4dae98dd67ef66877057b169
chao <chao.li@usask.ca>
2020-12-01 18:44:38 +0800

    img bug fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,7 +43,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/static/img`,
+        path: `${__dirname}/src/images`,
         name: "images",
       },
     },
