99cda34d9fa8e2b2bee2a5c4752c61f85c2af576
chao <chao.li@usask.ca>
2021-01-29 11:03:36 +0800

    update install info

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,13 +35,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `imagesicon`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         path: `${__dirname}/src/images`,
         name: "images",
       },
